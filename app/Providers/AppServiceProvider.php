2eea993ca6cb8359cf639023c5b7b446a1d76e44
tonglyhour <82593341+tonglyhour@users.noreply.github.com>
2023-01-20 23:51:39 +0700

    sss

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if($this->app->environment('production')) {
-        \URL::forceScheme('https');
-    }
+            \URL::forceScheme('https');
+        }
     }
 }
